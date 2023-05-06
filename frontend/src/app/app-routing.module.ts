69e61f95b72d700badefb75f28af212468c28b87
Chris-AS1 <8493773+Chris-AS1@users.noreply.github.com>
2023-05-06 15:58:28 +0200

    Fixed paths

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 import { authGuard } from './auth.guard';
 import { cashierGuard } from './guards/cashier/cashier.guard';
 import { CashierComponent } from './components/cashier/cashier.component';
@@ -11,14 +9,16 @@ import { WaiterComponent } from './components/waiter/waiter.component';
 import { cookGuard } from './guards/cook/cook.guard';
 import { bartenderGuard } from './guards/bartender/bartender.guard';
 import { waiterGuard } from './guards/waiter/waiter.guard';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  {path: "", component: HomeComponent},
-  {path: "login", component: LoginComponent},
-  {path: "cashier", component: CashierComponent, canActivate: [authGuard, cashierGuard]},
-  {path: "cook", component: CookComponent, canActivate: [authGuard, cookGuard]},
-  {path: "bartender", component: BartenderComponent, canActivate: [authGuard, bartenderGuard]},
-  {path: "waiter", component: WaiterComponent, canActivate: [authGuard, waiterGuard]},
+  { path: "", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "cashier", component: CashierComponent, canActivate: [authGuard, cashierGuard] },
+  { path: "cook", component: CookComponent, canActivate: [authGuard, cookGuard] },
+  { path: "bartender", component: BartenderComponent, canActivate: [authGuard, bartenderGuard] },
+  { path: "waiter", component: WaiterComponent, canActivate: [authGuard, waiterGuard] },
 ];
 
 @NgModule({
