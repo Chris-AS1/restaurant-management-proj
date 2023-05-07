af336284a0e0f191bd1b993a89090fe114a055b8
Chris-AS1 <8493773+Chris-AS1@users.noreply.github.com>
2023-05-07 11:25:15 +0200

    Misc fixes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { authGuard } from './auth.guard';
-import { cashierGuard } from './guards/cashier/cashier.guard';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { CashierComponent } from './components/cashier/cashier.component';
 import { CookComponent } from './components/cook/cook.component';
 import { BartenderComponent } from './components/bartender/bartender.component';
 import { WaiterComponent } from './components/waiter/waiter.component';
+import { authGuard } from './auth.guard';
+import { cashierGuard } from './guards/cashier/cashier.guard';
 import { cookGuard } from './guards/cook/cook.guard';
 import { bartenderGuard } from './guards/bartender/bartender.guard';
 import { waiterGuard } from './guards/waiter/waiter.guard';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
