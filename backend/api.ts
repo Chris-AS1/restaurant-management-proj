5b68af2f2c9c4beafb404aa9028d284b51bb1bb1
Chris-AS1 <8493773+Chris-AS1@users.noreply.github.com>
2023-05-07 11:25:00 +0200

    Initialized API structure

--- backend/api.ts
+++ backend/api.ts
@@ -1,5 +1,15 @@
-import { express } from "express";
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import express from 'express'
 
-
+// Loads .env
+dotenv.config()
 const app = express()
+const port = process.env["API_PORT"] || 3000
 
+app.get('/', (req, res) => {
+    res.send('Hello World!!!!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
