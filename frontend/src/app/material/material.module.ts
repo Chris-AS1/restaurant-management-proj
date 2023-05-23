65da04c39f93cb38f8061efd8a24ef7cc426edd0
Chris-AS1 <8493773+Chris-AS1@users.noreply.github.com>
2023-05-23 17:56:44 +0200

    Added material list component

--- frontend/src/app/material/material.module.ts
+++ frontend/src/app/material/material.module.ts
@@ -4,13 +4,16 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatListModule } from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
 const MaterialComponents = [
   MatRadioModule,
   MatInputModule,
   MatButtonModule,
   MatCheckboxModule,
   MatCardModule,
+  MatListModule,
+  MatIconModule,
 ]
 
 @NgModule({
