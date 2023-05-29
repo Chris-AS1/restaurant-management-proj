719c7bed1ddbe1dd81c883d0a8ced17d8994ed63
Chris-AS1 <8493773+Chris-AS1@users.noreply.github.com>
2023-05-29 11:41:13 +0200

    Refactoring, better error handling

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,7 +1,5 @@
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import express from 'express'
-import { LoginResponse } from './models/responses/login.response.model';
-import { genToken, getUser } from './db/auth';
 import { environment } from './environment.dev';
 import { expressjwt as jwt } from 'express-jwt';
 
@@ -24,39 +22,25 @@ app.get('/', (req, res) => {
     res.send('')
 })
 
-app.post('/login', async (req, res) => {
-    pino.info(req.body)
-    const dummy_res: LoginResponse = {
-        success: true,
-        role: 2,
-        token: genToken("jeffffry"),
-    }
-    res.send(dummy_res)
-
-    // TODO encrypt password, verify password
-    const cur_user = await getUser(req.body.username)
-    if (cur_user) {
-        const hash_psw = cur_user.password
-        const unk_psw = req.body.password // TODO hash
-        if (hash_psw === unk_psw) {
-            const login_res: LoginResponse = {
-                success: true,
-                role: cur_user.role,
-                token: genToken(req.body.username),
-            }
-
-            res.send(login_res)
-        }
-    }
-})
-
 // protecting routes
 app.use("/cashier", jwt({ secret: environment.JWT_KEY, algorithms: ["HS256"] }));
 app.use("/cook", jwt({ secret: environment.JWT_KEY, algorithms: ["HS256"] }));
 app.use("/bartender", jwt({ secret: environment.JWT_KEY, algorithms: ["HS256"] }));
 app.use("/waiter", jwt({ secret: environment.JWT_KEY, algorithms: ["HS256"] }));
 
+// @ts-ignore
+app.use((err, req, res, next) => {
+    if (err.name === "UnauthorizedError") {
+        res.status(401).send("Invalid Token.");
+    } else {
+        next(err);
+    }
+})
+
 // import routes
+var auth = require('./routes/auth')
+app.use("/", auth)
+
 var cashier = require('./routes/cashier')
 app.use('/cashier', cashier)
 
---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,31 @@
+import express from "express"
+import { genToken, getUser } from '../db/auth';
+import { NormalResponse } from "../models/responses/normal.response.model";
+import { LoginResponse } from "../models/responses/login.response.model";
+
+
+var router = express.Router();
+
+router.post('/login', async (req, res) => {
+    // TODO encrypt password, verify password
+    const cur_user = await getUser(req.body.username)
+    if (cur_user) {
+        const hash_psw = cur_user.password
+        const unk_psw = req.body.password // TODO hash
+        if (hash_psw === unk_psw) {
+            const login_res: LoginResponse = {
+                success: true,
+                role: cur_user.role,
+                token: genToken(req.body.username),
+            }
+
+            res.send(login_res)
+        }
+    } else {
+        res.status(401)
+        res.send({ success: false, message: "Wrong credentials" } as NormalResponse)
+    }
+})
+
+module.exports = router;
+
